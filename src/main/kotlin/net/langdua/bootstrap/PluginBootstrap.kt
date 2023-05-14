cbb1570aee36ad3a0dbbbb1a9c92187b5b955715
tudubucket <tumanh1511@gmail.com>
2023-05-14 10:25:35 +0700

    fix: bstats not working properly

--- src/main/kotlin/net/langdua/bootstrap/PluginBootstrap.kt
+++ src/main/kotlin/net/langdua/bootstrap/PluginBootstrap.kt
@@ -1,8 +1,6 @@
 package net.langdua.bootstrap
 
 import net.kyori.adventure.text.minimessage.MiniMessage
-import net.langdua.main.BuildConfig
-import org.bstats.bukkit.Metrics
 import org.bukkit.event.Listener
 import org.bukkit.plugin.java.JavaPlugin
 
